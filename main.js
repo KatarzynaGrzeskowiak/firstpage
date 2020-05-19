7007bce4d2260e7fb105b98a9192682e5ef14da9
Katarzyna Grześkowiak <tabita33@gmail.com>
2020-05-19 20:29:18 +0200

    Tried querySelector

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 27;
 alert('Witam :) ');
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const text = document.querySelector('.section-article__text--js');
+text.innerHTML = 'Tu miał być inny tekst ale go zmieniłam za pomocą js'
+
+console.log(text);
+
