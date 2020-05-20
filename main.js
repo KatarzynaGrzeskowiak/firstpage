4172a7f3e4484c07136057c35d2856bf5b6f5a89
Katarzyna Grześkowiak <tabita33@gmail.com>
2020-05-20 19:41:49 +0200

    Changes in formatting

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const name = 'Kasia';
+const name = "Kasia";
 const age = 27;
 
-alert('Witam :) ');
+alert("Witam :) ");
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
 
-const text = document.querySelector('.section-article__text--js');
-text.innerHTML = 'Tu miał być inny tekst ale go zmieniłam za pomocą js'
+const text = document.querySelector(".section-article__text--js");
+text.innerHTML = "Tu miał być inny tekst ale go zmieniłam za pomocą js";
 
 console.log(text);
-
