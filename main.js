cdb4d525de452ff365bfe88b87ad442ed175c13d
Katarzyna Grześkowiak <tabita33@gmail.com>
2020-05-23 19:17:36 +0200

    Changed from section-article to article. Added list

--- main.js
+++ main.js
@@ -5,7 +5,16 @@ alert("Witam :) ");
 
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
 
-const text = document.querySelector(".section-article__text--js");
-text.innerHTML = "Tu miał być inny tekst ale go zmieniłam za pomocą js";
+const text = document.querySelector(`.section-article__text--js`);
+text.innerHTML = `Tu miał być inny tekst ale go zmieniłam za pomocą js`;
 
 console.log(text);
+
+function calculate(myNumber){
+    console.log(`Dostałam ${myNumber}.`);
+    return myNumber*7;
+}
+
+const myResult = calculate(3);
+
+console.log(myResult);
